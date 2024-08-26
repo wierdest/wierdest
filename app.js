e6c185954055afbd927af93d2ed284756291eda0
andre <andrlzpt@protonmail.com>
2024-08-26 12:59:05 -0300

    passei a criação do websocket para o www, parece ser mais correto

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
+// parte desse código é gerado automaticamente por express {nome do proj} --view=pug
+// conferir:
+// https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/skeleton_website#the_generated_project
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const http = require('http');
-const { WebSocketServer } = require('ws');
-const uuid = require('uuid');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -36,40 +36,6 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
   res.render('error');
-});
-
-// Create HTTP server
-const server = http.createServer(app);
-
-// Create WebSocket server
-const wss = new WebSocketServer({ noServer: true });
-
-server.on('upgrade', function(request, socket, head) {
-  wss.handleUpgrade(request, socket, head, function(ws) {
-    const userId = uuid.v4();
-    ws.userId = userId; // Attach userId to the WebSocket connection
-
-    console.log('User connected with ID ', userId);
-
-    wss.emit('connection', ws, request);
-  });
-});
-
-wss.on('connection', function(ws) {
-
-  ws.send(JSON.stringify({ type: 'init', userId: ws.userId }));
-  ws.on('message', function(message) {
-    console.log(`Received message ${message} from user ${ws.userId}`);
-  });
-
-  ws.on('close', function() {
-    console.log(`User ${ws.userId} disconnected`);
-  });
-});
-
-// Start the server
-server.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running on http://localhost:${server.address().port}`);
 });
 
 module.exports = app;
